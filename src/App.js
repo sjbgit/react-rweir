e82c6fd05c41ca3081a25f201a23692d40d8dc44
sjbgit <sjb_strat@hotmail.com>
2022-11-18 21:46:17 +0000

    remove commented out code

--- src/App.js
+++ src/App.js
@@ -2,35 +2,6 @@ import { useState, useEffect, useRef, useReducer  } from "react";
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
-// const initialStories = [
-//   {
-//     title: 'React',
-//     url: 'https://reactjs.org/',
-//     author: 'Jordan Walke',
-//     num_comments: 3,
-//     points: 4,
-//     objectID: 0,
-//   },
-//   {
-//     title: 'Redux',
-//     url: 'https://redux.js.org/',
-//     author: 'Dan Abramov, Andrew Clark',
-//     num_comments: 2,
-//     points: 5,
-//     objectID: 1,
-//   },
-// ];
-
-// const getAsyncStories = () =>
-//   new Promise((resolve, reject) => {
-//     //setTimeout(reject, 2000);
-//     setTimeout(
-//       () => resolve({ data: { stories: initialStories } }),
-//       2000
-//     )
-//     //resolve({ data: { stories: initialStories } })
-//   });
-
 const storiesReducer = (state, action) => {
   switch (action.type) {
     case 'STORIES_FETCH_INIT':
@@ -62,15 +33,6 @@ const storiesReducer = (state, action) => {
     default:
       throw new Error();
   }
-  // if (action.type === 'SET_STORIES') {
-  //   return action.payload
-  // }
-  // else if (action.type === 'REMOVE_STORY') {
-  //   return state.filter((story) => action.payload.objectID !== story.objectID);
-  // }
-  // else {
-  //   throw new Error(); 
-  // }
 }
 
 const useStorageState = (key, initialState) => {
@@ -110,61 +72,15 @@ const App = () => {
     .catch(() =>{ dispatchStories({ type: 'STORIES_FETCH_FAILURE' })  })
   }, [searchTerm]);
 
-  // useEffect(() => {
-
-  //   if (!searchTerm) return
-  //   //setIsLoading(true)
-  //   dispatchStories({ type: 'STORIES_FETCH_INIT' });
-  //   fetch(`${API_ENDPOINT}${searchTerm}`)
-  //   .then((response) => response.json())
-  //   .then((result) => 
-  //   { 
-  //     console.log(result);
-  //       dispatchStories({
-  //       type: 'STORIES_FETCH_SUCCESS',
-  //       payload: result.hits,
-  //     });
-  //   })
-  //   .catch(() =>{ dispatchStories({ type: 'STORIES_FETCH_FAILURE' })  })
-
-  //   // getAsyncStories().then(result => {
-  //   //   //setStories(result.data.stories)
-  //   //   //dispatchStories({ type: 'SET_STORIES', payload: result.data.stories})
-  //   //   dispatchStories({
-  //   //     type: 'STORIES_FETCH_SUCCESS',
-  //   //     payload: result.data.stories,
-  //   //   });
-  //   //   //setIsLoading(false)
-  //   // }).catch(() =>{ dispatchStories({ type: 'STORIES_FETCH_FAILURE' })  }) //setIsError(true));
-  // }, [searchTerm]);
-
- 
-
-  // useEffect(() => {
-
-  // }, [searchTerm])
-
-  //const [isLoading, setIsLoading] = useState(false);
   const [stories, dispatchStories] = useReducer(storiesReducer, { data: [], isLoading: false, isError: false } );
-  //const [isError, setIsError] = useState(false);
-
+  
   const handleRemoveStory = (item) => {
     dispatchStories({ type: 'REMOVE_STORY', payload: item})
-    // const newStories = stories.filter(
-    //   (story) => item.objectID !== story.objectID
-    // );
-    // dispatchStories({ type: 'SET_STORIES', newStories})
-    // //setStories(newStories);
   };
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-
-  // const searchedStories = stories.data.filter((story) =>
-  // {
-  //   return story.title.toLowerCase().includes(searchTerm.toLowerCase());
-  // });
 
   return (
     <div>
@@ -255,93 +171,3 @@ const Item = ({ item, onRemoveItem }) => (
 );
 
 export default App
-
-
-// // import './App.css';
-
-// import { useEffect } from "react";
-// import { useState } from "react";
-
-// //THIS KEEPS GETTING CALLED WHEN REACT RENDERS - ALWAYS PASSING THE 
-// //INITIAL KEY AND THE INITIAL STATE
-// const useStorageState = (key, initialState) => {
-//   const [value, setValue] = useState(localStorage.getItem(key) || initialState);
-//   useEffect(() => {
-//     localStorage.setItem(key, value) //THIS RECEIVES THE KEY AND UPDATES SEARCH CRITERIA
-//   }, [key,value])
-
-//   return [value, setValue];
-// }
-
-// const initialStories = [
-//   {
-//     title: 'React',
-//     url: 'https://reactjs.org/',
-//     author: 'Jordan Walke',
-//     num_comments: 3,
-//     points: 4,
-//     objectID: 0,
-//   },
-//   {
-//     title: 'Redux',
-//     url: 'https://redux.js.org/',
-//     author: 'Dan Abramov, Andrew Clark',
-//     num_comments: 2,
-//     points: 5,
-//     objectID: 1,
-//   },
-// ];  
-
-// const App = () => {
-
-
-
-//   const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
-//   const [stories, setStories] = useStorageState(initialStories);
-
-
-//   const handleSearch = (event) => {
-//     setSearchTerm(event.target.value);
-//   }
-
-//   const searchedStories = stories.filter(story => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
-
-
-//   return (
-//     <div>
-//       <h1>My Hacker Stories</h1>
-//       <InputWithLabel isFocused id="search" value={searchTerm} onInputChange={handleSearch}><strong>Search:</strong></InputWithLabel>
-//       <br/>
-//       <List list={searchedStories}/>
-//     </div>
-//   );
-// }
-
-// const InputWithLabel = ({ id, value, type ='text', onInputChange, isFocused, children }) => (
-//   <>
-//     <label htmlFor={id}>{ children }</label>&nbsp;
-//     <input autoFocus={isFocused} id={id} type={type} value={value} onChange={onInputChange}/>
-//   </>
-// );
-
-// const List = ({ list }) => (
-//   <ul>
-//     {list.map((item) => (
-//       <Item key={item.objectID} item={item}/>
-//     ))}
-//   </ul>
-// );
-
-// const Item = ({ item }) => (
-//   <li>
-//     <span>
-//       <a href={item.url}>{item.title}</a>
-//     </span>
-//     <span>{item.author}</span>
-//     <span>{item.num_comments}</span>
-//     <span>{item.points}</span>
-//   </li>
-// )
-
-
-// export default App;
